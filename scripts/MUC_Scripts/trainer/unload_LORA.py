f16d36e1b54f1a1b8474d72acae8be5b774b05a1
Mikel Zubillaga Areitio <mikel.zubillaga.areitio@gmail.com>
2025-04-20 19:43:46 +0200

    Unload lora added

---
+++ scripts/MUC_Scripts/trainer/unload_LORA.py
@@ -0,0 +1,25 @@
+from peft import PeftModel
+from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed
+from vllm import LLM, SamplingParams
+import torch
+import argparse
+
+#Argument parser
+parser = argparse.ArgumentParser(description='Arguments required')
+parser.add_argument('--model-name',dest="model_name", type=str, help='Model name')
+parser.add_argument('--chkpt-path',dest="chkpt_path", type=str, help='Checkpoint path')
+parser.set_defaults(model_name="/scratch/ehu_p518_1/ehu_p518_1_1/Ereduak/DeepSeek-R1-Distill-Llama-70B")
+
+args = parser.parse_args()
+model_name = args.model_name
+chkpt_path = args.chkpt_path
+
+merge_path = "/scratch/ehu_p518_1/ehu_p518_1_1/SFT/Merged_Model"
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype= torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Path to save the merged model
+# Merge LoRA and base model and save
+peft_model = PeftModel.from_pretrained(model, chkpt_path)
+merged_model = peft_model.merge_and_unload()
+merged_model.save_pretrained(merge_path,safe_serialization=True)
+tokenizer.save_pretrained(merge_path)
